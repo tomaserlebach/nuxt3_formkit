22eed7850e3695b5797aa77e09c898abf5b84db7
Tomas Erlebach <terlebach1984@gmail.com>
2023-09-12 22:36:56 +0200

    removed test object

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,11 +1,6 @@
 import { DefaultConfigOptions, createInput } from "@formkit/vue";
 import Test from "~/custom_form_types/test.vue";
 
-const test = {
-  type: "input",
-  schema: ["Hello world2"],
-};
-
 const config: DefaultConfigOptions = {
   inputs: {
     test: createInput(Test)
